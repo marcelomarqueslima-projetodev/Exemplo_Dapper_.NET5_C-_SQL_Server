76833ccbd5a510cc2cfef7437e4a02eeab87a146
Marcelo Marques de Lima <marcelomarqueslima.mm@gmail.com>
2021-08-25 18:23:20 -0300

    Iniciandoestrutura do projeto console para estudo ADO.NET

---
+++ DataAccess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionsString = "server=DESKTOP-297NQ9F\\SQLEXPRESS;Database=Blog; User ID=sa;Password=mar15l82";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
