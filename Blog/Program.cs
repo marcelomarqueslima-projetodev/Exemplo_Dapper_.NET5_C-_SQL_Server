ebd6f2f9715bdd37d6b9f0835044ddc29ab475ef
Marcelo Marques de Lima <marcelomarqueslima.mm@gmail.com>
2021-08-27 11:54:22 -0300

    Estruturando o projeto do Dapper

---
+++ Blog/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=DESKTOP-297NQ9F\\SQLEXPRESS;Database=Blog;User ID=sa;Password=mar15l82";
+
+        static void Main(string[] args)
+        {
+            var Connection = new SqlConnection(CONNECTION_STRING);
+            Connection.Open();
+
+            Connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
--- Blog/obj/Debug/net5.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net5.0/Blog.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
+[assembly: System.Reflection.AssemblyProductAttribute("Blog")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
