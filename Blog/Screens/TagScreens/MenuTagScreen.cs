2fbc1f1feb92836da65a21e235b1cdc5ab4f8ccc
Marcelo Marques de Lima <marcelomarqueslima.mm@gmail.com>
2021-08-28 10:03:13 -0300

    Estruturando Estudos em Dapper

---
+++ Blog/Screens/TagScreens/MenuTagScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Blog.Screens.TagScreens
+{
+    public static class MenuTagScreen
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Gestão de tags");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Lista tags");
+            Console.WriteLine("2 - Cadastrar tags");
+            Console.WriteLine();
+            Console.WriteLine();
+            var option = short.Parse(Console.ReadLine()!);
+
+            switch (option)
+            {
+                case 1:
+                    Load();
+                    break;
+                case 2:
+                    Load();
+                    break;
+                default: Load(); break;
+            }
+        }
+    }
+}
---
+++ MsDataAccess/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace MsDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=DESKTOP-297NQ9F\\SQLEXPRESS;Database=Blog;User ID=sa;Password=mar15l82";
+
+            var connection = new SqlConnection();
+
+            connection.Open();
+            connection.Close();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
